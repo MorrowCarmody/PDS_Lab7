c260e7c63ba4ab46fde5c7aa1ab41f19abba970e
matt_carmody <matt.carmody@ovsgroup.com>
2023-06-26 11:51:22 -0500

    begin to add functionality

--- Lab7_Part2.py
+++ Lab7_Part2.py
@@ -2,7 +2,17 @@
 # COSC 6375
 # Lab 7 Part 2
 
+import numpy as np
 
+arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+
+np_arr = np.array(arr)
+
+n = int(len(arr)/4)
+
+np_arr = np_arr.reshape(n,4)
+
+print(np_arr)
 
 """
 INSTRUCTIONS:
